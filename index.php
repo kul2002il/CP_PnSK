c948cd137cfc076fc7bcda5dba7ecd7413f7befc
Кулманаков Илья Владимирович <gr482_kiv@ttit.local>
2021-04-07 14:12:37 +0700

    Сделана функция прошаривания директории.

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+	function read_folder($folder)
+	{
+		$files = scandir($folder);
+		$out = [];
+		foreach($files as $file)
+		{
+			if ($file == '.' || $file == '..') continue;
+			$fullfilename = $folder . '/' . $file;
+			if (is_dir($fullfilename))
+				$out = array_merge($out, read_folder($fullfilename));
+			else
+				array_push($out, $fullfilename);
+		}
+		return $out;
+	}
+
+	$sourses = read_folder("apps");
+?>
+<!doctype html>
+<html lang="ru" xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Даб 1</title>
+	<script src="js/vue.js" defer></script>
+	<script src="js/main.js" defer></script>
+</head>
+<body id="hostAppCompile">
+	<?php
+		echo "dir:\n";
+		print_r($sourses);
+	?>
+</body>
+</html>
--- js/main.js
+++ js/main.js
@@ -1,58 +1,6 @@
 /*
-let app = new Vue({
-	el: "#app",
-	data: {
-		message: "Начало.",
-	},
-});
-
-
-let app2 = new Vue({
-	el: "#app2",
-	data: {
-		message: "Вы загрузили эту страницу: " + new Date().toLocaleString(),
-	},
-});
-
-
-let app3 = new Vue({
-	el: "#app3",
-	data: {
-		seen: true,
-	},
-});
-
-
-let app4 = new Vue({
-	el: "#app4",
-	data: {
-		todo:
-		[
-			{text: "Начать"},
-			{text: "Продолжить"},
-			{text: "Закончить"},
-		],
-	},
-});
-
-
-let app5 = new Vue({
-	el: "#app5",
-	data:
-	{
-		message: "Другое начало.",
-	},
-	methods:
-	{
-		reverse: function () {
-			this.message = this.message.split('').reverse().join("");
-		}
-	},
-});
-*/
-
 let username = new Vue({
-	el: "#username",
+	el: "#hostAppCompile",
 	data: {
 		login: true,
 		message: "",
@@ -70,52 +18,4 @@ let username = new Vue({
 	},
 });
 
-Vue.component("todo-item", {
-	props: ["todo"],
-	data: function()
-	{
-		return {
-			exist: true,
-			line: false,
-		}
-	},
-	template:
-		'<li v-if="exist">' +
-		'<span v-on:click="lineGo">' +
-		'<span v-if="!line">{{todo}}</span>' +
-		'<del v-if="line">{{todo}}</del>' +
-		'</span>' +
-		'<button v-on:click="deleteGo">Удалить</button>' +
-		'</li>',
-	methods:
-	{
-		deleteGo: function(){
-			this.exist = false;
-		},
-		lineGo: function(){
-			this.line = !this.line;
-		}
-	},
-});
-
-/*
-let app7 = new Vue({
-	el: "#app7",
-	data: {
-		list: [
-			{
-				id: 0,
-				text: "Текст0",
-			},
-			{
-				id: 1,
-				text: "Текст1",
-			},
-			{
-				id: 2,
-				text: "Текст2",
-			},
-		],
-	},
-});
 */
